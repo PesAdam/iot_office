3a15f3f006417b5bc7b7f083a790eab8e093f152
Martin Synák <martin.synak123@gmail.com>
2024-05-30 06:35:32 +0200

    upraveny BTN, zabezpecenie page

--- web/resources/views/card/kitchen.blade.php
+++ web/resources/views/card/kitchen.blade.php
@@ -8,29 +8,34 @@
     </div>
     
     <div class="f">
-        <div class="card">
-            <h2>CO2</h2>
-            <h3>100pm</h3>
-            <img src="images/SVGRepo_iconCarrier (1).svg">
-        </div>
-        
-        <div class="card">
-            <h2>LPG</h2>
-            <h3>100pm</h3>
-            <img src="images/Group.svg">
-        </div>
-        
-        <div class="card">
-            <h2>Teplota</h2>
-            <h3>100pm</h3>
-            <img src="images/Group (1).svg">
-        </div>
-        
-        <div class="card">
-            <h2>Vlhkosť</h2>
-            <h3>100pm</h3>
-            <img src="images/SVGRepo_iconCarrier.svg">
-        </div>
+        <a href="#">
+            <div class="card">
+                <h2>CO2</h2>
+                <h3>100pm</h3>
+                <img src="images/SVGRepo_iconCarrier (1).svg">
+            </div>
+        </a>
+        <a href="#">
+            <div class="card">
+                <h2>LPG</h2>
+                <h3>100pm</h3>
+                <img src="images/Group.svg">
+            </div>
+        </a>
+        <a href="#">
+            <div class="card">
+                <h2>Teplota</h2>
+                <h3>100pm</h3>
+                <img src="images/Group (1).svg">
+            </div>
+        </a>
+        <a href="#">
+            <div class="card">
+                <h2>Vlhkosť</h2>
+                <h3>100pm</h3>
+                <img src="images/SVGRepo_iconCarrier.svg">
+            </div>
+        </a>
     </div>
 </div>
 
--- web/resources/views/card/safety.blade.php
+++ web/resources/views/card/safety.blade.php
@@ -7,32 +7,55 @@
     display: flex;
     justify-content: space-between;
     padding: 20px;
-    border: 1px solid #ccc;
     border-radius: 8px;
-    background-color: #f9f9f9;
 }
 
 .sensors-date{
-    display: flex
+    display: flex;
+    gap: 3rem;    
 }
 
 /* Box containing sensor status */
 .sensors {
     flex: 1;
-    margin-right: 20px;
-    padding: 10px;
-    border: 1px solid #ddd;
+    padding: 1rem;
     border-radius: 8px;
-    background-color: #ffffff;
+    background-color: #D9D9D9;
+    width: 30rem;
+}
+
+.sensors-header {
+    display: flex;
+    justify-content: space-around;
+    margin-bottom: 1rem;
+}
+
+.sensors-header p {
+    font-weight: bold;
+    font-size: 1.25rem;
+}
+
+.sensors-body {
+    display: flex;
+    justify-content: space-around;
+}
+
+.sensors-body div p .green{
+    font-weight: 900;
+    color: #07c807;
+}
+
+.sensors-body div p .red{
+    font-weight: 900;
+    color: #ff0000;
 }
 
 /* Box containing time information */
 .time {
-    flex: 2;
+    flex: 1;
     padding: 10px;
-    border: 1px solid #ddd;
     border-radius: 8px;
-    background-color: #ffffff;
+    background-color: #D9D9D9;
 }
 
 /* Styling for paragraphs inside sensors and time boxes */
@@ -63,9 +86,25 @@
     <div class="f">
         <div class="sensors-date">
             <div class="sensors">
-                <p>Objekt 1: aktivny</p>
-                <p>Objekt 2: aktivny</p>
-                <p>Objekt 3: aktivny</p>
+                <div class="sensors-header">
+                    <p>Senzor</p>
+                    <p>Status</p>
+                </div>
+                <div class="sensors-body">
+                    <div>
+                        <p>Objekt 1</p>
+                        <p>Objekt 2</p>
+                        <p>Objekt 3</p> 
+                    </div>
+                    <div>
+                        <p><span class="green">·</span> Aktivny</p>
+                        <p><span class="green">·</span> Aktivny</p>
+                        <p><span class="green">·</span> Aktivny</p>
+                    </div>
+    
+                </div>
+                
+                
             </div>
             <div class="time">
                 <h2>Security 1</h2>
